42df9ba305a572aae0d89367328164760b11870b
SH <SH@example.com>
2020-05-22 12:31:19 +0900

    add drone manage system

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+import config
+
+import droneapp.controllers.server
+
+logging.basicConfig(level=logging.INFO,
+                    #filename=config.LOG_FILE
+                    stream=sys.stdout)
+
+if __name__ == '__main__':
+    droneapp.controllers.server.run()
